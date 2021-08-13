29d8edcb26d1dc299f5edbde4182d9eec817eed5
Arthur Bomfim Neto <arthur.bomfim.n@gmail.com>
2021-08-13 10:16:01 -0300

    Criacao dos controllers de seguidores

--- api/src/controllers/controllers_usuarios.go
+++ api/src/controllers/controllers_usuarios.go
@@ -2,9 +2,12 @@ package controllers
 
 import (
 	//"api/src/banco"
+
+	"api/src/autenticacao"
 	"api/src/banco"
 	modelos "api/src/models"
 	"api/src/repositorios"
+	"errors"
 	"strconv"
 	"strings"
 
@@ -104,6 +107,23 @@ func GetUsuario(w http.ResponseWriter, r *http.Request) {
 
 //PutUsuario atualiza um usuario no banco
 func PutUsuarios(w http.ResponseWriter, r *http.Request) {
+	parametros := mux.Vars(r)
+	id, erro := strconv.ParseUint(parametros["usuarioId"], 10, 64)
+	if erro != nil {
+		respostas.Erro(w, http.StatusBadRequest, erro)
+		return
+	}
+
+	usuarioIdToken, erro := autenticacao.ExtrairusarioID(r)
+	if erro != nil {
+		respostas.Erro(w, http.StatusUnauthorized, erro)
+	}
+
+	if usuarioIdToken != id {
+		respostas.Erro(w, http.StatusForbidden, errors.New("Não é possivel atualizar um usuario sem ser o seu"))
+		return
+	}
+
 	corpoRequisicao, erro := ioutil.ReadAll(r.Body)
 	if erro != nil {
 		respostas.Erro(w, http.StatusUnprocessableEntity, erro)
@@ -113,13 +133,6 @@ func PutUsuarios(w http.ResponseWriter, r *http.Request) {
 	var usuario modelos.Usuario
 
 	erro = json.Unmarshal(corpoRequisicao, &usuario)
-	if erro != nil {
-		respostas.Erro(w, http.StatusBadRequest, erro)
-		return
-	}
-
-	parametros := mux.Vars(r)
-	id, erro := strconv.ParseUint(parametros["usuarioId"], 10, 64)
 	if erro != nil {
 		respostas.Erro(w, http.StatusBadRequest, erro)
 		return
@@ -159,6 +172,17 @@ func DeleteUsuario(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	usuarioIdToken, erro := autenticacao.ExtrairusarioID(r)
+	if erro != nil {
+		respostas.Erro(w, http.StatusUnauthorized, erro)
+		return
+	}
+
+	if usuarioIdToken != id {
+		respostas.Erro(w, http.StatusForbidden, errors.New("Não é possivel deletar um usuario sem ser o seu"))
+		return
+	}
+
 	db, erro := banco.Conectar()
 	if erro != nil {
 		respostas.Erro(w, http.StatusInternalServerError, erro)
@@ -171,4 +195,102 @@ func DeleteUsuario(w http.ResponseWriter, r *http.Request) {
 	erro = repositorio.DeletarUser(id)
 
 	respostas.JSON(w, http.StatusNoContent, nil)
+}
+
+func PostSeguidor(w http.ResponseWriter, r *http.Request) {
+	parametros := mux.Vars(r)
+	usuarioID, erro := strconv.ParseUint(parametros["usuarioId"], 10, 64)
+	if erro != nil {
+		respostas.Erro(w, http.StatusBadRequest, erro)
+		return
+	}
+
+	seguidorID, erro := autenticacao.ExtrairusarioID(r)
+	if erro != nil {
+		respostas.Erro(w, http.StatusUnauthorized, erro)
+		return
+	}
+
+	if seguidorID == usuarioID {
+		respostas.Erro(w, http.StatusForbidden, errors.New("Não é possivel seguir você mesmo"))
+		return
+	}
+
+	db, erro := banco.Conectar()
+	if erro != nil {
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+		return
+	}
+
+	defer db.Close()
+
+	repositorio := repositorios.NovoRepositorioDeUsuarios(db)
+	erro = repositorio.Seguir(usuarioID, seguidorID)
+	if erro != nil {
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+	}
+
+	respostas.JSON(w, http.StatusNoContent, nil)
+
+}
+
+func DeleteSeguidor(w http.ResponseWriter, r *http.Request) {
+	parametros := mux.Vars(r)
+	usuarioID, erro := strconv.ParseUint(parametros["usuarioId"], 10, 64)
+	if erro != nil {
+		respostas.Erro(w, http.StatusBadRequest, erro)
+		return
+	}
+
+	seguidorID, erro := autenticacao.ExtrairusarioID(r)
+	if erro != nil {
+		respostas.Erro(w, http.StatusUnauthorized, erro)
+		return
+	}
+
+	if seguidorID == usuarioID {
+		respostas.Erro(w, http.StatusForbidden, errors.New("Não é possivel deixar de seguir você mesmo"))
+		return
+	}
+
+	db, erro := banco.Conectar()
+	if erro != nil {
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+	}
+
+	defer db.Close()
+
+	repositorio := repositorios.NovoRepositorioDeUsuarios(db)
+	erro = repositorio.ParaDeSeguir(usuarioID, seguidorID)
+	if erro != nil {
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+	}
+
+	respostas.JSON(w, http.StatusNoContent, nil)
+}
+
+func GetSeguidores(w http.ResponseWriter, r *http.Request) {
+	parametros := mux.Vars(r)
+	usuarioID, erro := strconv.ParseUint(parametros["usuarioId"], 10, 64)
+	if erro != nil {
+		respostas.Erro(w, http.StatusBadRequest, erro)
+		return
+	}
+
+	db, erro := banco.Conectar()
+	if erro != nil {
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+		return
+	}
+
+	defer db.Close()
+
+	repositorio := repositorios.NovoRepositorioDeUsuarios(db)
+	seguidores, erro := repositorio.BuscarSeguidores(usuarioID)
+	if erro != nil {
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+		return
+	}
+
+	respostas.JSON(w, http.StatusOK, seguidores)
 }
