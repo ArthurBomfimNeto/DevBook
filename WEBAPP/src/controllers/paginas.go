249bf2c848016457b92cadd1c4497a38a79ee093
Arthur Bomfim Neto <arthur.bomfim.n@gmail.com>
2022-07-27 16:43:00 -0300

    Commit teste

--- WEBAPP/src/controllers/paginas.go
+++ WEBAPP/src/controllers/paginas.go
@@ -5,12 +5,12 @@ import (
 	"webapp/src/utils"
 )
 
-//CarregarTelaDeLogin renderixa a tela de login
+//CarregarTelaDeLogin renderiza a tela de login
 func CarregarTelaDeLogin(w http.ResponseWriter, r *http.Request) {
 	utils.ExecutarTemplate(w, "login.html", nil)
 }
 
-//CarregarPaginaDeCadastroDeUsuario ...
+//CarregarPaginaDeCadastroDeUsuario  renderiza a tela de cadastro de usuario
 func CarregarPaginaDeCadastroDeUsuario(w http.ResponseWriter, r *http.Request) {
 	utils.ExecutarTemplate(w, "cadastro.html", nil)
 }
