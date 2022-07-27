04312098f1e0dc8b635daf3e3bc77a5fd9eccaac
Arthur Bomfim Neto <arthur.bomfim.n@gmail.com>
2022-07-27 16:39:33 -0300

    Commit teste

--- WEBAPP/src/controllers/paginas.go
+++ WEBAPP/src/controllers/paginas.go
@@ -10,6 +10,7 @@ func CarregarTelaDeLogin(w http.ResponseWriter, r *http.Request) {
 	utils.ExecutarTemplate(w, "login.html", nil)
 }
 
+//CarregarPaginaDeCadastroDeUsuario ...
 func CarregarPaginaDeCadastroDeUsuario(w http.ResponseWriter, r *http.Request) {
 	utils.ExecutarTemplate(w, "cadastro.html", nil)
 }
