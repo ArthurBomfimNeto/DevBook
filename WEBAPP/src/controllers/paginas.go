70bf89971036170baececb99c881a5c165f092cb
Arthur Bomfim Neto <arthur.bomfim.n@gmail.com>
2022-07-27 16:31:07 -0300

    Criação da tela e rota de cadastro de usuário

--- WEBAPP/src/controllers/paginas.go
+++ WEBAPP/src/controllers/paginas.go
@@ -9,3 +9,7 @@ import (
 func CarregarTelaDeLogin(w http.ResponseWriter, r *http.Request) {
 	utils.ExecutarTemplate(w, "login.html", nil)
 }
+
+func CarregarPaginaDeCadastroDeUsuario(w http.ResponseWriter, r *http.Request) {
+	utils.ExecutarTemplate(w, "cadastro.html", nil)
+}
